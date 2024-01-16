e2c91a2df7023cbe896cc74fb8d1822df2fe73de
ehmenzala <ehmenzala@gmail.com>
2024-01-16 14:51:50 -0500

    fix: remove incorrect imports

--- biblioweb/books/admin.py
+++ biblioweb/books/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 
-from .models import Question, Choice, Author, Book, Genre, Cover, Award
+from .models import Author, Book, Genre, Cover, Award
 
-admin.site.register(Question)
-admin.site.register(Choice)
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Genre)
