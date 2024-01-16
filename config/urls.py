70ecdb15f7f6b0f46bd5a629094837dfd15a15d9
ehmenzala <ehmenzala@gmail.com>
2024-01-16 14:53:07 -0500

    feat(books): add index view for books

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', include('biblioweb.books.urls')),
     path('', include('biblioweb.main.urls')),
 ]
