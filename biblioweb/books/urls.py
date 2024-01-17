a68444b0fbb30f1ab60c911ed06b43abe41e7c57
ehmenzala <ehmenzala@gmail.com>
2024-01-17 16:13:33 -0500

    feat(books): add books and detail of book view

--- biblioweb/books/urls.py
+++ biblioweb/books/urls.py
@@ -5,4 +5,5 @@ app_name = 'books'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:book_id>/', views.book, name='detail'),
 ]
--- biblioweb/books/views.py
+++ biblioweb/books/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, Http404
+from django.shortcuts import render
+from .models import Book
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('Index Books')
+    books = Book.objects.all()
+    return render(request, 'books.html', {"books": books})
+
+
+def book(request: HttpRequest, book_id: int) -> HttpResponse:
+    try:
+        book = Book.objects.get(pk=book_id)
+    except Book.DoesNotExist:
+        raise Http404("This book does not exist.")
+    return render(request, 'book-detail.html', {"book": book})
