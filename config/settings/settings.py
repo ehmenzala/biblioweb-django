7706465e21e0a390dd3989de89acc1b6f6c22470
ehmenzala <ehmenzala@gmail.com>
2024-01-18 13:26:55 -0500

    feat(root): add STATIC_ROOT config

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -104,6 +104,7 @@ USE_TZ = False  # UTC
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
