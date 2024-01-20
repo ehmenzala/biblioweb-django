234b4b4427cda70fc115c98519c856289465f417
ehmenzala <ehmenzala@gmail.com>
2024-01-20 11:34:30 -0500

    feat(books): add book genre view

--- biblioweb/books/urls.py
+++ biblioweb/books/urls.py
@@ -4,6 +4,10 @@ from . import views
 app_name = 'books'
 
 urlpatterns = [
+    # ex: /books/
     path('', views.index, name='index'),
+    # ex: /books/5
     path('<int:book_id>/', views.book, name='detail'),
+    # ex: /books/genre/1
+    path('genre/<int:genre_id>/', views.genre, name='genre'),
 ]
--- biblioweb/books/views.py
+++ biblioweb/books/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse, HttpRequest, Http404
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Genre
 
 
 def index(request: HttpRequest) -> HttpResponse:
     books = Book.objects.all()
-    return render(request, 'books.html', {"books": books})
+    genres = Genre.objects.all()
+    context = {
+        "books": books,
+        "genres": genres
+    }
+    return render(request, 'books.html', context)
 
 
 def book(request: HttpRequest, book_id: int) -> HttpResponse:
@@ -14,3 +19,20 @@ def book(request: HttpRequest, book_id: int) -> HttpResponse:
     except Book.DoesNotExist:
         raise Http404("This book does not exist.")
     return render(request, 'book-detail.html', {"book": book})
+
+
+def genre(request: HttpRequest, genre_id: int) -> HttpResponse:
+    genres = Genre.objects.all()
+    books = Book.objects.filter(genres__id=genre_id)
+
+    try:
+        selected_genre = Genre.objects.get(pk=genre_id)
+    except Genre.DoesNotExist:
+        raise Http404("This genre does not exist.")
+
+    context = {
+        "books": books,
+        "genres": genres,
+        "selected_genre": selected_genre,
+    }
+    return render(request, 'book-genre.html', context)
