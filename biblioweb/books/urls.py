323c631f555726dca8c8e90f83b87cc4bd6cef27
ehmenzala <ehmenzala@gmail.com>
2024-02-12 23:35:33 -0500

    chore(books): remove useless help comments

--- biblioweb/books/urls.py
+++ biblioweb/books/urls.py
@@ -4,10 +4,7 @@ from . import views
 app_name = 'books'
 
 urlpatterns = [
-    # ex: /books/
     path('', views.index, name='index'),
-    # ex: /books/5
     path('<int:book_id>/', views.book, name='detail'),
-    # ex: /books/genre/1
     path('genre/<int:genre_id>/', views.genre, name='genre'),
 ]
