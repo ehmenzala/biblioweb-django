c0a52b6509914962c64f379cd5e4e5f056f068aa
ehmenzala <ehmenzala@gmail.com>
2024-02-12 23:34:59 -0500

    feat(account): add forms, views, and urls for account management

--- biblioweb/account/views.py
+++ biblioweb/account/views.py
@@ -1,3 +1,35 @@
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from .forms import LoginForm, RegistrationForm
 
-# Create your views here.
+
+def login(request: HttpRequest) -> HttpResponse:
+
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            # TODO: Start session here
+            return HttpResponseRedirect(reverse('main:index'))
+        else:
+            login_form = LoginForm(request.POST)
+            return render(request, 'account/login.html', {'form': login_form})
+
+    if request.method == 'GET':
+        login_form = LoginForm()
+        return render(request, 'account/login.html', {'form': login_form})
+
+
+def registration(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        registration_form = RegistrationForm(request.POST)
+        if registration_form.is_valid():
+            # TODO: Start session here
+            return HttpResponseRedirect(reverse('main:index'))
+        else:
+            registration_form = RegistrationForm(request.POST)
+            return render(request, 'account/registration.html', {'form': registration_form})
+
+    if request.method == 'GET':
+        registration_form = RegistrationForm()
+        return render(request, 'account/registration.html', {'form': registration_form})
--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('biblioweb.account.urls')),
     path('books/', include('biblioweb.books.urls')),
     path('', include('biblioweb.main.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
