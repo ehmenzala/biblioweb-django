948904c2924d07359989da39c88a2e2b642816d0
ehmenzala <ehmenzala@gmail.com>
2024-02-20 12:34:46 -0500

    feat(books): add sample read view for book

--- biblioweb/books/urls.py
+++ biblioweb/books/urls.py
@@ -6,5 +6,6 @@ app_name = 'books'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<slug:slug>/', views.detail, name='detail'),
+    path('read/<slug:slug>/', views.read, name='read'),
     path('genre/<int:genre_id>/', views.genre, name='genre'),
 ]
--- biblioweb/books/views.py
+++ biblioweb/books/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpRequest, Http404
+from django.http import HttpResponse, HttpRequest, Http404, HttpResponseBadRequest
 from django.shortcuts import render
 from .models import Book, Genre
 
@@ -36,3 +36,23 @@ def genre(request: HttpRequest, genre_id: int) -> HttpResponse:
         "selected_genre": selected_genre,
     }
     return render(request, 'book-genre.html', context)
+
+
+def read(request: HttpRequest, slug: str) -> HttpResponse:
+    page = request.GET.get('page')
+    chapter = request.GET.get('chapter')
+
+    if (page == None or chapter == None):
+        return HttpResponseBadRequest('400. Bad Request: URL parameters are invalid.')
+
+    try:
+        book = Book.objects.get(slug=slug)
+    except Book.DoesNotExist:
+        raise Http404("This book does not exist.")
+
+    context = {
+        "book": book,
+        "chapter": chapter,
+        "page": page
+    }
+    return render(request, 'book-read.html', context)
