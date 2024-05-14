4b96fabd890f3e8e2e2939fdeb6512268b076cb8
ehmenzala <ehmenzala@gmail.com>
2024-05-14 11:29:46 -0500

    chore(books): re-organize urls

--- biblioweb/books/urls.py
+++ biblioweb/books/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'books'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<slug:slug>/', views.detail, name='detail'),
     path('read/<slug:slug>/', views.read, name='read'),
     path('genre/<int:genre_id>/', views.genre, name='genre'),
+    path('<slug:slug>/', views.detail, name='detail'),
+    path('', views.index, name='index'),
 ]
