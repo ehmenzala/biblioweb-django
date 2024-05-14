846b354344d5d07bda11e92a4ee2aa88a75201ad
ehmenzala <ehmenzala@gmail.com>
2024-05-14 11:27:55 -0500

    feat(root): update static files configurations

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -104,7 +104,10 @@ USE_TZ = False  # UTC
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'biblioweb/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'biblioweb/staticprod')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'biblioweb/static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'biblioweb/media')
